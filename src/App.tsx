a0d4cd0e96782e43d7e04c37ad9736a9a4767e4e
Anton Skalaban <antonskalaban99@gmail.com>
2024-05-31 01:50:24 +0300

    fix: fix app

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "pages";
 import { Layout } from "components";
 
-// import "./App.scss";
+import "./App.scss";
 
-// export const App: FC = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Layout />}>
-//         <Route index element={<Home />} />
-//       </Route>
-//     </Routes>
-//   );
-// };
-const App = () => <h2>Hello next.js</h2>;
+export const App: FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
+};
+//const App = () => <h2>Hello next.js</h2>;
 
 export default App;
