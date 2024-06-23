b933135d78279b87be6c95b225a935c5d0e161d2
Anton Skalaban <antonskalaban99@gmail.com>
2024-06-23 15:04:52 +0300

    fix: fix tests config

--- jest.config.mjs
+++ jest.config.mjs
@@ -4,21 +4,10 @@ const createJestConfig = nextJest({
   dir: "./",
 });
 
-
-
 /** @type {import('jest').Config} */
 const config = {
   testEnvironment: "jest-environment-jsdom",
   testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
-  // moduleNameMapper: {
-  //   // Workaround to put our SVG stub first
-  //   "\\.svg": "<rootDir>/../../tools/jest/svg-stub.js",
-  //   // ...nextJestConfig.moduleNameMapper,
-  // },
-  // transform: {
-  //   // "^.+\\.tsx?$": "ts-jest",
-  //   "^.+\\.svg$": "<rootDir>/svgTransform.cjs",
-  // },
 };
 
 export default createJestConfig(config);
